be01e213655e018b33773a13edf5a494ae67c4a3
Brent Laabs <bslaabs@gmail.com>
2013-04-17 23:51:37 -0700

    switch to "handles" delegation for methods

--- lib/File/Spec.pm
+++ lib/File/Spec.pm
@@ -1,28 +1,37 @@
 class File::Spec;
 my $module;
 my %module = (
-	'MacOS'   => 'Mac',
-	<MSWin32 os2 dos NetWare symbian> »=>» 'Win32',
-	'VMS'     => 'VMS',
-	'cygwin'  => 'Cygwin',
-	# in case someone passes a module name instead of an OS string
-	#  map it to themselves
-	<Unix Mac Win32 Cygwin> »xx» 2
+	<MacOS Mac>                             »=>» 'Mac',
+	<MSWin32 os2 dos NetWare symbian Win32> »=>» 'Win32',
+	<cygwin Cygwin>                         »=>» 'Cygwin',
+	'VMS'     => 'VMS'
 );
 
 $module = "File::Spec::" ~ (%module{$*OS} // 'Unix');
 require ::($module);
+my $CLASS = ::($module);
+
+method MODULE handles
+	<canonpath curdir updir rootdir devnull tmpdir
+	 file-name-is-absolute no-parent-or-current-test
+	 path split join splitpath catpath catfile
+	 splitdir catdir abs2rel rel2abs NYI>
+		 { $CLASS }
+
+method os (Str $OS = $*OS ) {
+	my $module = "File::Spec::" ~ (%module{$OS} // 'Unix');
+	require ::($module);
+	::($module);
+}
+
+=begin pod
 
 #| MODULE - for module introspection
 method MODULE                            { $module; }  # for introspection
 
 #| Returns a copy of the module for the given OS string
 #| e.g. File::Spec.os('Win32') returns File::Spec::Win32
-method os (Str $OS = $*OS ) {
-	my $module = "File::Spec::" ~ (%module{$OS} // 'Unix');
-	require ::($module);
-	::($module);
-}
+
 
 # class methods
 method canonpath( $path )                   { ::($module).canonpath( $path )                   }
@@ -45,3 +54,4 @@ method rel2abs( |c )                        { ::($module).rel2abs( |c )         
 method split ( $path )                       { ::($module).split( $path )                      }
 method join ( $volume, $directory, $file )   { ::($module).join( $volume, $directory, $file )  }
 
+=end pod
